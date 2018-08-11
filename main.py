8b66c01ca43abe7420e1f312a8127aa0509e6f0d
Shaked Dovrat <shaked.dovrat@gmail.com>
2018-08-11 21:45:05 +0300

    cleanup

--- main.py
+++ main.py
@@ -61,58 +61,20 @@ class DefectDetector:
         self.valid_registration_mask = None
 
     def run(self):
-        # self._pre_process()
         self._register()
-        # cv2.medianBlur(self.reference_image_registered, self.MEDIAN_FILTER_SIZE, self.reference_image_registered)
-        # cv2.medianBlur(self.inspection_image, self.MEDIAN_FILTER_SIZE, self.inspection_image)
         diff_image = self._diff()
-        # cv2.medianBlur(diff_image, self.MEDIAN_FILTER_SIZE, diff_image)
         joint_edges_mask = self._joint_edges()
         JOINT_EDGES_FACTOR = 1 / 3
         diff_image[joint_edges_mask] = diff_image[joint_edges_mask] * JOINT_EDGES_FACTOR
-        show_image(diff_image, 'diff image lower joint edges')
+        if self.debug:
+            show_image(diff_image, 'diff image lower joint edges')
         valid_diff_mask = self._diff_binarization(diff_image)
         output_mask = self._post_process(valid_diff_mask)
-        show_image(output_mask, 'output_mask')
+        if self.debug:
+            show_image(output_mask, 'output_mask')
 
         print('valid_diff_mask mean = {}'.format(np.mean(valid_diff_mask.flatten())))
         plt.close('all')
-        return
-
-        # Second type of defects:
-
-        # LOW_DIFF_THRESHOLD = 1.3
-        # HIGH_DIFF_THRESHOLD = 1.7
-        # diff_image = relative_diff(self.reference_image_registered, self.inspection_image)
-        # diff_mask = apply_hysteresis_threshold(diff_image, LOW_DIFF_THRESHOLD, HIGH_DIFF_THRESHOLD)
-        # valid_diff_mask = np.bitwise_and(diff_mask, self.valid_registration_mask)
-        # if self.debug:
-        #     show_image(diff_image)
-        #     show_image(valid_diff_mask)
-        valid_diff_mask = self._diff()
-
-        edges = cv2.Canny(self.inspection_image, 100, 200)
-        MORPHOLOGY_SE_SIZE = (5, 5)
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, MORPHOLOGY_SE_SIZE)
-        cv2.dilate(edges, kernel, edges)
-        show_image(edges)
-
-        diff_no_edges = np.logical_and(valid_diff_mask, np.logical_not(edges))
-        show_image(diff_no_edges, 'diff no edges')
-
-        plt.close('all')
-
-    def _pre_process(self):
-        # if self.debug:
-        show_image(self.inspection_image)
-        cv2.medianBlur(self.reference_image, self.MEDIAN_FILTER_SIZE, self.reference_image)
-        cv2.medianBlur(self.inspection_image, self.MEDIAN_FILTER_SIZE, self.inspection_image)
-        # GAUSS_FILTER_SIZE = (0, 0)  # 0 means compute from sigma
-        # GAUSS_SIGMA = 3
-        # cv2.GaussianBlur(self.reference_image, GAUSS_FILTER_SIZE, GAUSS_SIGMA, self.reference_image)
-        # cv2.GaussianBlur(self.inspection_image, GAUSS_FILTER_SIZE, GAUSS_SIGMA, self.inspection_image)
-        if self.debug:
-            show_image(self.inspection_image)
 
     def _register(self):
         CROSS_CORELLATION = True
@@ -126,54 +88,6 @@ class DefectDetector:
             alinger.find_allignment()
         self.reference_image_registered = alinger.transform(self.reference_image)
         self.valid_registration_mask = alinger.get_valid_mask(self.reference_image.shape)
-
-        # fine-tune translation (sub-pixel):
-        FINE_TUNE = False
-        if FINE_TUNE:
-            def sliding_window(image, window_size, y_valid_range, x_valid_range):
-                dm = divmod(window_size, 2)
-                assert dm[1] == 1
-                half_window = dm[0]
-                # slide a window across the image
-                for y in range(max(half_window, y_valid_range[0]), min(image.shape[0]-half_window, y_valid_range[1])):
-                    for x in range(max(half_window, x_valid_range[0]), min(image.shape[1]-half_window, x_valid_range[1])):
-                        # yield the current window
-                        yield (x, y, image[y-half_window:y + half_window + 1, x-half_window:x + half_window + 1])
-
-            a1 = np.where(self.valid_registration_mask)
-            y_valid_range, x_valid_range = [[a2.min(), a2.max()] for a2 in a1]
-
-            FILTER_SIZE = 5
-            A = []
-            b = []
-            for x, y, window in sliding_window(self.reference_image_registered, FILTER_SIZE, y_valid_range, x_valid_range):
-                A.append(window.flatten())
-                b.append(self.inspection_image[y, x])
-
-            SCIPY_LSTSQ = True
-            if SCIPY_LSTSQ:
-                LOSS = 'soft_l1'#'cauchy'
-                from scipy.optimize import least_squares
-                def fun(x, A, b):
-                    # return x[0] + x[1] * np.exp(x[2] * t) - y
-                    return b - np.matmul(A, x)
-                x0 = np.zeros((FILTER_SIZE, FILTER_SIZE)).flatten()
-                x0[int(x0.size/2)] = 1.0
-                res_log = least_squares(fun, x0, loss=LOSS, args=(np.array(A), np.array(b)))
-                x = res_log.x
-            else:
-                x, residuals, rank, s = np.linalg.lstsq(A, b)
-
-            filter = x.reshape(FILTER_SIZE, FILTER_SIZE)
-            if self.debug:
-                show_image(filter, 'filter')
-
-            dst = cv2.filter2D(self.reference_image_registered, -1, filter)
-            if self.debug:
-                show_image(self.reference_image_registered, 'translation')
-                show_image(dst, 'subpixel translation')
-            self.reference_image_registered = dst
-            pass
 
     def _diff(self):
         DIFF = 'abs'#''normed'
